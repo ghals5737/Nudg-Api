fd9aa84b93b685efe5931e2b9c44fe44ea9ff349
ghals5737 <ghals5737@naver.com>
2025-12-11 18:03:52 +0900

    add controller

---
+++ src/main/kotlin/com/example/nudgapi/controller/routines/RoutineController.kt
@@ -0,0 +1,9 @@
+package com.example.nudgapi.controller.routines
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/routines")
+class RoutineController {
+}
